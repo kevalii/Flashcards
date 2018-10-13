941d4b72cd63e1970a075deea2ca2c12595733a6
Alex R <alexrankine@college.harvard.edu>
2018-10-13 16:31:24 -0400

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Alex Rankine on 10/13/18.
+//  Copyright © 2018 Alex Rankine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
